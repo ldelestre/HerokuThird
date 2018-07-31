717807d99cd8d9be9686e07781c37bfbbe28f1ea
ldelestre <l98.broadhurst@gmail.com>
2018-07-31 15:01:37 +0200

    Dare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		user = User.create(username: params["username"], email: params["email"], bio: params["bio"])
+		if user.new_record? == true
+			redirect_to "/error"
+		else
+			redirect_to "/"
 	end
 end
