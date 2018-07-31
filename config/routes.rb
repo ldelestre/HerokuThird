c44d7ed7936db4cda51c5a74729f0f1f509418f2
ldelestre <l98.broadhurst@gmail.com>
2018-07-31 14:07:23 +0200

    Dare

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get '/', to: 'static_pages#home'
+	get '/new', to: 'users_controller#new'
+	post '/new', to: 'users_controller#create'
+end
